47075d1863ec91d507fb2635f9a439d1677399c0
Inaridiy <inainainariinainari@gmail.com>
2022-12-24 23:56:28 +0900

    seo

--- packages/interface/src/pages/_app.tsx
+++ packages/interface/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { NextSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
 import { polygon } from "wagmi/chains";
@@ -36,16 +37,38 @@ const wagmiClient = createClient({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={new QueryClient()}>
-      <WagmiConfig client={wagmiClient}>
-        <RainbowKitProvider chains={chains}>
-          <HomeLayout>
-            <NoSSR>
-              <Component {...pageProps} />
-            </NoSSR>
-          </HomeLayout>
-        </RainbowKitProvider>
-      </WagmiConfig>
-    </QueryClientProvider>
+    <>
+      <NextSeo
+        title="Xmas NFT Trade Event"
+        description="Everyone bring your NFT's and let's give each other Christmas presents!!!"
+        canonical="https://nft-mistery-box-interface.vercel.app/"
+        openGraph={{
+          url: "https://nft-mistery-box-interface.vercel.app/",
+          title: "Xmas NFT Trade Event",
+          description:
+            "Everyone bring your NFT's and let's give each other Christmas presents!!!",
+          images: [
+            { url: "https://nft-mistery-box-interface.vercel.app/ogp.jpg" },
+          ],
+          siteName: "Xmas NFT Trade Event",
+        }}
+        twitter={{
+          handle: "@inaridiy",
+          site: "@site",
+          cardType: "summary_large_image",
+        }}
+      />
+      <QueryClientProvider client={new QueryClient()}>
+        <WagmiConfig client={wagmiClient}>
+          <RainbowKitProvider chains={chains}>
+            <HomeLayout>
+              <NoSSR>
+                <Component {...pageProps} />
+              </NoSSR>
+            </HomeLayout>
+          </RainbowKitProvider>
+        </WagmiConfig>
+      </QueryClientProvider>
+    </>
   );
 }
